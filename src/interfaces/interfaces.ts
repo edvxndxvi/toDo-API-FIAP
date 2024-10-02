3bd3df9b0626114d0c3f774da5708820d0edb74e
edvxndxvi <edvandavi1@hotmail.com>
2024-10-02 14:33:06 -0300

    Adicionando arquivo interfaces.ts com as definições das interfaces Todo e Target

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,14 @@
+export interface Todo {
+    id: number;
+    title: string;
+    description: string;
+    isComplete: boolean;
+    targetId: number;
+}
+  
+export interface Target {
+    id: number;
+    title: string;
+    description: string;
+    isComplete: boolean;
+}
